8cb7a21a8faec338576ca457e439b45bcbe4ae83
Waleed Yaser <waleedyaser95@gmail.com>
2019-01-15 07:08:27 +0200

    Implement linked stack

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
+#include "stack.h"
+#include "linked_stack.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STACK_ENTRY int
-#include "stack.h"
 
 void draw(STACK_ENTRY e) {
 	printf("drawing \"%d\"...\n", e);
 }
 
+void test_stack(void);
+void test_lstack(void);
+
 int main()
 {
+	test_stack();
+	test_lstack();
+}
+
+void test_stack(void)
+{
+	printf("===== stack test =====\n");
 	Stack s;
 	STACK_ENTRY e;
 	init_stack(&s);
@@ -44,4 +55,50 @@ int main()
 	}
 
 	traverse_stack(&s, draw);
+	clear_stack(&s);
+	printf("stack %s\n", stack_empty(&s) ? "cleared" : "not cleared");
+	printf("======================\n");
+}
+
+void test_lstack(void)
+{
+	printf("===== linked stack test =====\n");
+	LStack ls;
+	LSTACK_ENTRY e;
+	init_lstack(&ls);
+	for (int i = 0; i < 5; ++i) {
+		e = i;
+		if (!lstack_full(&ls))
+			ls_push(e, &ls);
+		else {
+			fprintf(stderr, "stack is full!");
+			exit(1);
+		}
+	}
+
+	while (!lstack_empty(&ls)) {
+		ls_pop(&e, &ls);
+		printf("%d\n", e);
+	}
+
+	for (int i = 0; i < 10; ++i) {
+		e = i;
+		if (!lstack_full(&ls))
+			ls_push(e, &ls);
+		else {
+			fprintf(stderr, "stack is full!");
+			exit(1);
+		}
+	}
+	
+	if (!lstack_empty(&ls)) {
+		lstack_top(&e, &ls);
+		printf("stack top is: %d\n", e);
+	}
+
+	traverse_lstack(&ls, draw);
+
+	clear_lstack(&ls);
+	printf("linked stack %s\n", lstack_empty(&ls) ? "cleared" : "not cleared");
+	printf("=============================\n");
 }
